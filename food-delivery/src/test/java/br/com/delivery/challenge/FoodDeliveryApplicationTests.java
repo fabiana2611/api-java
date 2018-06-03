1aa90fa21c5ff361335f7f2019d1bdb83732dd9e
fabiana <fabiana2611@hotmail.com>
2018-06-03 20:29:12 -0300

    Create new branch

---
+++ food-delivery/src/test/java/br/com/delivery/challenge/FoodDeliveryApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.delivery.challenge;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FoodDeliveryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
